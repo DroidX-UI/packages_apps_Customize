d2db053d2175f7e515fd4cba4b88da513078d6f0
NRanjan-17 <nalinishranjan05@gmail.com>
2025-05-05 01:18:51 +0530

    Revert "Settings: Add a toggle for Settings UI Styles [1/2]"
    This reverts commit 61ee869d2c0c01ce2fd5636c3b0621da6064dbfd.

--- src/com/droidx/customize/System.java
+++ src/com/droidx/customize/System.java
@@ -33,8 +33,6 @@ import androidx.preference.PreferenceScreen;
 import androidx.preference.SwitchPreference;
 
 import com.android.internal.logging.nano.MetricsProto;
-import com.android.internal.util.android.SystemRestartUtils;
-import com.droidx.support.preferences.SystemSettingListPreference;
 
 import com.android.settings.R;
 import com.android.settings.SettingsPreferenceFragment;
@@ -50,10 +48,6 @@ import java.util.List;
 public class System extends SettingsPreferenceFragment 
             implements Preference.OnPreferenceChangeListener {
 
-    public static final String SETTINGS_DASHBOARD_STYLE = "settings_dashboard_style";
- 
-    private SystemSettingListPreference mSettingsDashBoardStyle;
-
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
@@ -61,18 +55,10 @@ public class System extends SettingsPreferenceFragment
         PreferenceScreen prefSet = getPreferenceScreen();
         final Resources res = getResources();
         final PreferenceScreen prefScreen = getPreferenceScreen();
-        mSettingsDashBoardStyle = (SystemSettingListPreference) findPreference(SETTINGS_DASHBOARD_STYLE);
-        mSettingsDashBoardStyle.setOnPreferenceChangeListener(this);
     }
     
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final String key = preference.getKey();
-        ContentResolver resolver = getActivity().getContentResolver();
- 	    if (preference == mSettingsDashBoardStyle){
-            SystemRestartUtils.showSettingsRestartDialog(getContext());
-            return true;
-            }
         return false;
     }  
 
