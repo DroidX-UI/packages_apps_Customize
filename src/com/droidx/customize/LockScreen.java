EpicROM-AOSP/android_packages_apps_Customize

Customize: Introduce AOD styles [2/2]

src/com/epic/settings/fragments/lockscreen/LockScreen.java
@@ -5,12 +5,19 @@
 
 package com.epic.settings.fragments.lockscreen;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.UserHandle;
+import android.provider.MediaStore;
+import android.provider.Settings;
 import android.text.TextUtils;
+import android.widget.Toast;
 
 import androidx.preference.Preference;
 import androidx.preference.Preference.OnPreferenceChangeListener;
@@ -26,6 +33,7 @@ import com.android.settingslib.search.SearchIndexable;
 import java.util.List;
 
 import com.epic.settings.preferences.SecureSettingSwitchPreference;
+import com.epic.settings.utils.ImageUtils;
 
 @SearchIndexable
 public class LockScreen extends SettingsPreferenceFragment implements
@@ -38,7 +46,10 @@ public class LockScreen extends SettingsPreferenceFragment implements
     private static final String KEY_SCREEN_OFF_UDFPS = "screen_off_udfps_enabled";
     private static final String KEY_AUTHENTICATION_SUCCESS = "fp_success_vibrate";
     private static final String KEY_AUTHENTICATION_ERROR = "fp_error_vibrate";
+    private static final String CUSTOM_IMAGE_REQUEST_CODE_KEY = "lockscreen_custom_image";
+    private static final int CUSTOM_IMAGE_REQUEST_CODE = 1001;
 
+    private Preference mCustomImagePreference;
     private PreferenceCategory mFingerprintCategory;
     private SecureSettingSwitchPreference mScreenOffUdfps;
 
@@ -51,6 +62,17 @@ public class LockScreen extends SettingsPreferenceFragment implements
         final ContentResolver resolver = context.getContentResolver();
         final PreferenceScreen prefScreen = getPreferenceScreen();
         final Resources resources = context.getResources();
+
+        mCustomImagePreference = findPreference(CUSTOM_IMAGE_REQUEST_CODE_KEY);
+        int clockStyle = Settings.Secure.getIntForUser(getContext().getContentResolver(), "clock_style", 0, UserHandle.USER_CURRENT);
+        String imagePath = Settings.System.getString(getContext().getContentResolver(), "custom_aod_image_uri");
+        if (imagePath != null && clockStyle > 0) {
+            mCustomImagePreference.setSummary(imagePath);
+            mCustomImagePreference.setEnabled(true);
+        } else if (clockStyle == 0) {
+            mCustomImagePreference.setSummary(getContext().getString(R.string.custom_aod_image_not_supported));
+            mCustomImagePreference.setEnabled(false);
+        }
 
         mFingerprintCategory = (PreferenceCategory) findPreference(KEY_FINGERPRINT_CATEGORY);
         mScreenOffUdfps = (SecureSettingSwitchPreference) findPreference(KEY_SCREEN_OFF_UDFPS);
@@ -77,6 +99,37 @@ public class LockScreen extends SettingsPreferenceFragment implements
         final Context context = getContext();
         final ContentResolver resolver = context.getContentResolver();
         return false;
+    }
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCustomImagePreference) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, CUSTOM_IMAGE_REQUEST_CODE);
+            } catch(Exception e) {
+                Toast.makeText(getContext(), R.string.quick_settings_header_needs_gallery, Toast.LENGTH_LONG).show();
+            }
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
+        if (requestCode == CUSTOM_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK && result != null) {
+            Uri imgUri = result.getData();
+            if (imgUri != null) {
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "lockscreen_aod_image", "LOCKSCREEN_CUSTOM_AOD_IMAGE");
+                if (savedImagePath != null) {
+                    ContentResolver resolver = getContext().getContentResolver();
+                    Settings.System.putStringForUser(resolver, "custom_aod_image_uri", savedImagePath, UserHandle.USER_CURRENT);
+                    mCustomImagePreference.setSummary(savedImagePath);
+                }
+            }
+        }
     }
 
     @Override

DroidX-UI/packages_apps_Customize

Signed-off-by: NRanjan-17 <nalinishranjan05@gmail.com>

src/com/droidx/customize/LockScreen.java
@@ -15,17 +15,22 @@
  */
 package com.droidx.customize;
 
+import android.app.Activity;
 import android.content.ContentResolver;
 import android.content.Context;
+import android.content.Intent;
 import android.content.res.Resources;
 import android.hardware.fingerprint.FingerprintManager;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.UserHandle;
 import android.text.TextUtils;
 import android.provider.SearchIndexableResource;
+import android.provider.MediaStore;
 import android.provider.Settings;
+import android.widget.Toast;
 
 import androidx.preference.ListPreference;
 import androidx.preference.Preference;
@@ -47,6 +52,7 @@ import java.util.Arrays;
 import java.util.List;
 
 import com.droidx.support.preferences.SecureSettingSwitchPreference;
+import com.droidx.support.utils.ImageUtils;
 
 @SearchIndexable
 public class LockScreen extends SettingsPreferenceFragment 
@@ -54,7 +60,10 @@ public class LockScreen extends SettingsPreferenceFragment
 
     private static final String KEY_FINGERPRINT_CATEGORY = "lock_screen_fingerprint_category";
     private static final String KEY_SCREEN_OFF_UDFPS = "screen_off_udfps_enabled";
+    private static final String CUSTOM_IMAGE_REQUEST_CODE_KEY = "lockscreen_custom_image";
+    private static final int CUSTOM_IMAGE_REQUEST_CODE = 1001;
 
+    private Preference mCustomImagePreference;
     private PreferenceCategory mFingerprintCategory;
     private SecureSettingSwitchPreference mScreenOffUdfps;
     
@@ -68,6 +77,17 @@ public class LockScreen extends SettingsPreferenceFragment
         final ContentResolver resolver = context.getContentResolver();
         final Resources res = context.getResources();
         final PreferenceScreen prefScreen = getPreferenceScreen();
+
+        mCustomImagePreference = findPreference(CUSTOM_IMAGE_REQUEST_CODE_KEY);
+        int clockStyle = Settings.Secure.getIntForUser(getContext().getContentResolver(), "clock_style", 0, UserHandle.USER_CURRENT);
+        String imagePath = Settings.System.getString(getContext().getContentResolver(), "custom_aod_image_uri");
+        if (imagePath != null && clockStyle > 0) {
+            mCustomImagePreference.setSummary(imagePath);
+            mCustomImagePreference.setEnabled(true);
+        } else if (clockStyle == 0) {
+            mCustomImagePreference.setSummary(getContext().getString(R.string.custom_aod_image_not_supported));
+            mCustomImagePreference.setEnabled(false);
+        }
 
         mFingerprintCategory = (PreferenceCategory) findPreference(KEY_FINGERPRINT_CATEGORY);
         mScreenOffUdfps = (SecureSettingSwitchPreference) findPreference(KEY_SCREEN_OFF_UDFPS);
@@ -93,6 +113,37 @@ public class LockScreen extends SettingsPreferenceFragment
     public boolean onPreferenceChange(Preference preference, Object newValue) {
         return false;
     }  
+
+    @Override
+    public boolean onPreferenceTreeClick(Preference preference) {
+        if (preference == mCustomImagePreference) {
+            try {
+                Intent intent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                intent.setType("image/*");
+                startActivityForResult(intent, CUSTOM_IMAGE_REQUEST_CODE);
+            } catch(Exception e) {
+                Toast.makeText(getContext(), R.string.quick_settings_header_needs_gallery, Toast.LENGTH_LONG).show();
+            }
+            return true;
+        }
+        return super.onPreferenceTreeClick(preference);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent result) {
+        super.onActivityResult(requestCode, resultCode, result);
+        if (requestCode == CUSTOM_IMAGE_REQUEST_CODE && resultCode == Activity.RESULT_OK && result != null) {
+            Uri imgUri = result.getData();
+            if (imgUri != null) {
+                String savedImagePath = ImageUtils.saveImageToInternalStorage(getContext(), imgUri, "lockscreen_aod_image", "LOCKSCREEN_CUSTOM_AOD_IMAGE");
+                if (savedImagePath != null) {
+                    ContentResolver resolver = getContext().getContentResolver();
+                    Settings.System.putStringForUser(resolver, "custom_aod_image_uri", savedImagePath, UserHandle.USER_CURRENT);
+                    mCustomImagePreference.setSummary(savedImagePath);
+                }
+            }
+        }
+    }
 
     @Override
     public int getMetricsCategory() {